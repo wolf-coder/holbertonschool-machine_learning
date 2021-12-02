4e33c85b5182965a7b38b46d5cbdfa173f7684d8
wolf-coder <cuore.fakhri@gmail.com>
2021-12-02 17:27:00 +0100

    fix pep

--- math/0x00-linear_algebra/12-bracin_the_elements.py
+++ math/0x00-linear_algebra/12-bracin_the_elements.py
@@ -3,14 +3,16 @@
 simply a module documentation line.
 """
 
+
 def np_elementwise(mat1, mat2):
     """
     that performs element-wise addition, subtraction, multiplication,
     and division:
     - You can assume that mat1 and mat2 can be interpreted as numpy.ndarrays
-    - You should return a tuple containing the element-wise sum, difference, product, and quotient, respectively
-    - You are not allowed to use any loops or conditional statements
-    - You can assume that mat1 and mat2 are never empty
+    - You should return a tuple containing
+    the element-wise sum, difference, product, and quotient, respectively.
+    - You are not allowed to use any loops or conditional statements.
+    - You can assume that mat1 and mat2 are never empty.
     """
     return ((mat1 + mat2), (mat1 - mat2),
-            (mat1 * mat2), (mat1 / mat2)))
+            (mat1 * mat2), (mat1 / mat2))
