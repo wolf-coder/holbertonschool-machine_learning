c661098c3130d0b52963ae7b829ca1adcf69a5d3
wolf-coder <cuore.fakhri@gmail.com>
2021-12-02 17:52:17 +0100

    14-saddle_up.py

---
+++ math/0x00-linear_algebra/14-saddle_up.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""
+simply a module documentation line.
+"""
+import numpy as np
+
+
+def np_matmul(mat1, mat2):
+    """
+    Function that performs matrix multiplication:
+    - You can assume that mat1 and mat2 are numpy.ndarrays
+    - You are not allowed to use any loops or conditional statements
+    - You may use: import numpy as np
+    - You can assume that mat1 and mat2 are never empty
+    """
+
+    return mat1.dot(mat2)
