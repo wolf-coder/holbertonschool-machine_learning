8462a77c759c8f1cead207952dd0ea666b553f34
wolf-coder <cuore.fakhri@gmail.com>
2021-12-02 14:12:02 +0100

    Another approach

--- math/0x00-linear_algebra/8-ridin_bareback.py
+++ math/0x00-linear_algebra/8-ridin_bareback.py
@@ -13,14 +13,10 @@ def mat_mul(mat1, mat2):
     - You must return a new matrix.
     - If the two matrices cannot be multiplied, return None.
     """
-    def Calcul(L, T):
-        """
-        Function that caluculate a row by column
-        as a part of matrix multiplication.
-        """
-        return L[0] * T[0] + L[1] * T[1]
 
     if len(mat1[0]) != len(mat2):
         return None
     else:
-        return [[Calcul(row, col) for col in zip(*mat2)] for row in mat1]
+        return [[sum(x * y for x, y in zip(row, col))
+                 for col in zip(*mat2)]
+                for row in mat1]
