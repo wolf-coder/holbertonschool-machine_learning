df2975263940d178d24c4ef9529607d1f745e540
wolf-coder <cuore.fakhri@gmail.com>
2021-12-09 21:50:21 +0100

    Write a function def poly_derivative(poly): that calculates the derivative of a polynomial

---
+++ math/0x02-calculus/10-matisse.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""
+derive the poly
+"""
+
+
+def poly_derivative(poly):
+    """
+    calculates the derivative of a polynomial.
+    the index of the list represents the power of x
+    that the coefficient belongs to.
+    """
+    if type(poly) is not list or len(poly) == 0:
+        return None
+    if len(poly) == 1:
+        return [0]
+    return [index * coefficient
+            for index, coefficient in enumerate(poly)
+            if coefficient]
