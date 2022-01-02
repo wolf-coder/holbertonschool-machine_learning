76ed1fa107d4cea7067397c360c8596943136ea3
wolf-coder <cuore.fakhri@gmail.com>
2022-01-02 16:36:41 +0100

    Calculate z_score and x_score

--- math/0x03-probability/normal.py
+++ math/0x03-probability/normal.py
@@ -5,10 +5,29 @@ Normal distribution
 
 
 class Normal():
-    """Documentation for Normal
-
+    """
+    Work with normal distribution.
     """
     def __init__(self, data=None, mean=0., stddev=1.):
+        """
+Class contructor def __init__(self, data=None, mean=0., stddev=1.):
+    data is a list of the data to be used to estimate the distribution
+    mean is the mean of the distribution
+    stddev is the standard deviation of the distribution
+    Sets the instance attributes mean and stddev
+        Saves mean and stddev as floats
+    If data is not given (i.e. None (be careful: not data has not the
+    same result as data is None))
+        Use the given mean and stddev
+        If stddev is not a positive value or equals to 0, raise a
+ValueError with the message stddev must be a positive value.
+    If data is given:
+        Calculate the mean and standard deviation of data
+        If data is not a list, raise a TypeError with the message
+    'data must be a list'
+        If data does not contain at least two data points, raise
+    a ValueError with the message data must contain multiple values
+        """
         self.mean = float(mean)
         self.stddev = float(stddev)
         if data is None:
@@ -23,3 +42,19 @@ class Normal():
             self.stddev = ((sum([(elm - self.mean) ** 2 for elm in data]) /
                             len(data))
                            ** 0.5)
+
+    def z_score(self, x):
+        """
+        Instance method that calculates the z-score of a given x-value
+        x is the x-value
+        Returns the z-score of x
+        """
+        return (x - self.mean) / self.stddev
+
+    def x_value(self, z):
+        """
+        instance method that calculates the x-value of a given z-score
+        z is the z-score
+        Returns the x-value of z
+        """
+        return (z * self.stddev) + self.mean
