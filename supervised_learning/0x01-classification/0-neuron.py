ee10126f432a93f675eed7389a3c259c33fcb331
wolf-coder <cuore.fakhri@gmail.com>
2022-01-13 13:58:45 +0100

    First layer

---
+++ supervised_learning/0x01-classification/0-neuron.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+"""
+Neuron
+"""
+import numpy as np
+
+
+class Neuron:
+    """
+    class Neuron that defines a single neuron performing binary classification
+    """
+
+    def __init__(self, nx):
+        """
+        """
+        if type(nx) is not int:
+            raise TypeError("the exception: nx must be an integer")
+        if nx < 1:
+            raise ValueError("the exception: nx must be a positive integer")
+        self.W = np.random.randn(1, nx)
+        self.b = 0
+        self.A = 0
