d0a5749e5ac2b2ca423e0e140262938bf8425c38
wolf-coder <cuore.fakhri@gmail.com>
2022-01-17 22:57:35 +0100

    fix pep

--- supervised_learning/0x04-error_analysis/3-specificity.py
+++ supervised_learning/0x04-error_analysis/3-specificity.py
@@ -12,9 +12,12 @@ import numpy as np
 def specificity(confusion):
     """
     Calculates the specificity for each class in a confusion matrix:
-    - `confusion` is a confusion numpy.ndarray of shape (classes, classes) where row indices represent the correct labels and column indices represent the predicted labels
+    - `confusion` is a confusion numpy.ndarray of shape (classes, classes)
+where row indices represent the correct labels and column indices
+represent the predicted labels
     - `classes` is the number of classes
-    * Returns: a numpy.ndarray of shape (classes,) containing the specificity of each class
+    * Returns: a numpy.ndarray of shape (classes,)containing the specificity
+of each class
     """
     TP = np.diag(confusion)
     FP = confusion.sum(0) - np.diag(confusion)
