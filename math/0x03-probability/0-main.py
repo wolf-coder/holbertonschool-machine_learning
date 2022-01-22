77fb2aa169a6be42e32d494a149747c37577f90c
wolf-coder <cuore.fakhri@gmail.com>
2022-01-22 22:20:52 +0100

    Initialize Poinsson

---
+++ math/0x03-probability/0-main.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+import numpy as np
+Poisson = __import__('poisson').Poisson
+
+np.random.seed(0)
+data = np.random.poisson(5., 100).tolist()
+p1 = Poisson(data)
+print('Lambtha:', p1.lambtha)
+
+p2 = Poisson(lambtha=5)
+print('Lambtha:', p2.lambtha)
+
+"""Expected
+$ ./0-main.py 
+Lambtha: 4.84
+Lambtha: 5.0
+"""
+
