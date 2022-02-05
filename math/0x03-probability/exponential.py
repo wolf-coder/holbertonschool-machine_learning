c3d447065e761c9ef7a9088dacdef7d618c44987
wolf-coder <cuore.fakhri@gmail.com>
2022-02-05 19:22:54 +0100

    exponential class

---
+++ math/0x03-probability/exponential.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+"""
+exponential distribution
+"""
+
+
+class Exponential:
+    """
+    Create a class Exponential that represents an
+    exponential distribution
+    """
+
+    pi = 3.1415926536
+    e = 2.7182818285
+
+    def __init__(self, data=None, lambtha=1.):
+        """
+        constructor method
+        """
+        lambtha = float(lambtha)
+        if data is None:
+            if lambtha < 0:
+                raise ValueError("lambtha must be a positive value")
+            self.lambtha = lambtha
+        else:
+            if type(data) is not list:
+                raise TypeError("data must be a list")
+            if len(data) < 2:
+                raise ValueError("data must contain multiple values")
+
+            """ maximum likelihood estimate for the rate parameter lambtha
+            """
+            self.lambtha = len(data) / sum(data)
