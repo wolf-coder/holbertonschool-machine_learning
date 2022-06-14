b6640b875ec0931a069ae0db98898ccc410a6fe5
wolf-coder <cuore.fakhri@gmail.com>
2022-06-14 15:40:45 +0100

    pep8 fix

--- math/0x00-linear_algebra/100-slice_like_a_ninja.py
+++ math/0x00-linear_algebra/100-slice_like_a_ninja.py
@@ -4,6 +4,12 @@ import numpy as np
 
 def np_slice(matrix, axes={}):
     """
+    Method that slices a matrix along specific axes:
+       - .You can assume that matrix is a numpy.ndarray
+       - .You must return a new numpy.ndarray
+       - .axes is a dictionary where the key is an axis to slice along
+ and the value is a tuple representing the slice to make along that axis
+       - .You can assume that axes represents a valid slice
     """
     Slice = ((slice(*axes[i]) if i in axes.keys() else slice(None, None, None))
              for i in range(matrix.ndim))
