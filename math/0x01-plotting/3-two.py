a4b74a0ae4d5705ba5081a2e817033818db442d8
Bakouti Fakhri <cuore.fakhri@gmail.com>
2022-06-28 14:27:12 +0100

    legend

---
+++ math/0x01-plotting/3-two.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+import numpy as np
+import matplotlib.pyplot as plt
+
+x = np.arange(0, 21000, 1000)
+r = np.log(0.5)
+t1 = 5730
+t2 = 1600
+y1 = np.exp((r / t1) * x)
+y2 = np.exp((r / t2) * x)
+
+# plotting
+plt.xlabel("Time (years)")
+plt.ylabel("labeled Fraction Remaining")
+plt.title("Exponential Decay of Radioactive Elements")
+plt.show()
