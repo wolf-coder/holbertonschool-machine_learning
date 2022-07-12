962c65879add98261db0f8eddcba9e7109f49dc9
Bakouti Fakhri <cuore.fakhri@gmail.com>
2022-07-12 21:33:22 +0100

    gradient

---
+++ math/0x01-plotting/100-gradient.py
@@ -0,0 +1,21 @@
+
+#!/usr/bin/env python3
+import matplotlib.pyplot as plt
+import numpy as np
+
+np.random.seed(5)
+
+# Data
+x = np.random.randn(2000) * 10
+y = np.random.randn(2000) * 10
+z = np.random.rand(2000) + 40 - np.sqrt(np.square(x) + np.square(y))
+
+# Plotting
+cbar.set_label("elevation (m)" )
+plt.xlabel("x coordinate (m)")
+plt.title("Mountain Elevation")
+plt.ylabel("y coordinate (m)")
+plt.scatter(x, y, c=z)
+cbar = plt.colorbar()
+
+plt.show()
