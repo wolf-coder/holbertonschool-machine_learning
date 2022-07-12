85ca2864dd2636d132b287bf9bbc1940797d353b
wolf-coder <cuore.fakhri@gmail.com>
2022-07-12 22:16:38 +0100

    LeNet-5 (Tensorflow 1)

--- supervised_learning/0x07-cnn/4-lenet5.py
+++ supervised_learning/0x07-cnn/4-lenet5.py
@@ -9,7 +9,7 @@ def lenet5(x, y):
     """
     kernel = tf.contrib.layers.variance_scaling_initializer()
     L1_conv = tf.layers.conv2d(x, 6, (5, 5), kernel, "SAME", 'relu')
-    L2_pool = tf.layers.max_pooling2d(L1_conv, (2, 2),(2, 2))
+    L2_pool = tf.layers.max_pooling2d(L1_conv, (2, 2), (2, 2))
     L3_conv = tf.layers.conv2d(L2_pool, 16, (5, 5), kernel, 'relu')
     L4_pool = tf.layers.max_pooling2d(L3_conv, (2, 2), (2, 2))
     L4_flat = tf.contrib.layers.flatten(L4_pool)
@@ -19,5 +19,10 @@ def lenet5(x, y):
     Y_pred = tf.nn.softmax(L7_fc)
     loss = tf.losses.softmax_cross_entropy(y, L7_fc)
     Train_op = tf.train.AdamOptimizer().minimize(loss)
-    acc = tf.reduce_mean(tf.cast(tf.equal(tf.argmax(L7_fc, axis=1), tf.argmax(y, axis=1)), "float32"))
+    acc = tf.reduce_mean(
+        tf.cast(
+            tf.equal(
+                tf.argmax(
+                    L7_fc, axis=1), tf.argmax(
+                    y, axis=1)), "float32"))
     return Y_pred, Train_op, loss, acc
