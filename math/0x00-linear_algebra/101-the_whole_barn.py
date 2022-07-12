9507eb9950e0219bc12f7e11183e6220c3f6ef07
Bakouti Fakhri <cuore.fakhri@gmail.com>
2022-07-12 07:35:55 +0100

    recursive solution

--- math/0x00-linear_algebra/101-the_whole_barn.py
+++ math/0x00-linear_algebra/101-the_whole_barn.py
@@ -9,7 +9,8 @@ def add_matrices(mat1, mat2):
 - A function that adds two matrices:
 
     .You can assume that mat1 and mat2 are matrices containing ints/floats
-    .You can assume all elements in the same dimension are of the same type/shape
+    .You can assume all elements in the same dimension are of
+the same type/shape
     .You must return a new matrix
     .If matrices are not the same shape, return None
     .You can assume that mat1 and mat2 will never be empty
@@ -24,6 +25,6 @@ def add_matrices(mat1, mat2):
                 return None
             New_mat.append(New_row)
         return New_mat
-    except :
+    except Exception:
         """ reached the top of the matrix, """
         return mat1 + mat2
