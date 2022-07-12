56d0b68d659905f02269c3db73513d0837c3ce7f
wolf-coder <cuore.fakhri@gmail.com>
2022-07-13 00:33:21 +0100

    pep8

--- supervised_learning/0x07-cnn/4-lenet5.py
+++ supervised_learning/0x07-cnn/4-lenet5.py
@@ -8,7 +8,7 @@ def lenet5(x, y):
         modified version of the LeNet-5 architecture using tensorflow
     """
     kernel = tf.keras.initializers.VarianceScaling(scale=2.0)
-    L1_conv = tf.layers.conv2d(
+    L1 = tf.layers.conv2d(
         inputs=x,
         filters=6,
         kernel_size=(
@@ -17,41 +17,41 @@ def lenet5(x, y):
         kernel_initializer=kernel,
         padding="SAME",
         activation='relu')
-    L2_pool = tf.layers.max_pooling2d(
-        inputs=L1_conv, pool_size=(
+    L2 = tf.layers.max_pooling2d(
+        inputs=L1, pool_size=(
             2, 2), strides=(
             2, 2))
-    L3_conv = tf.layers.conv2d(
-        inputs=L2_pool,
+    L3 = tf.layers.conv2d(
+        inputs=L2,
         filters=16,
         kernel_size=(
             5,
             5),
         kernel_initializer=kernel,
         activation='relu')
-    L4_pool = tf.layers.max_pooling2d(
-        inputs=L3_conv, pool_size=(
+    L4 = tf.layers.max_pooling2d(
+        inputs=L3, pool_size=(
             2, 2), strides=(
             2, 2))
-    L4_flat = tf.layers.flatten(L4_pool)
-    L5_fc = tf.layers.dense(
-        inputs=L4_flat,
+    L4 = tf.layers.flatten(L4)
+    L5 = tf.layers.dense(
+        inputs=L4,
         units=120,
         kernel_initializer=kernel,
         activation='relu')
-    L6_fc = tf.layers.dense(
-        inputs=L5_fc,
+    L6 = tf.layers.dense(
+        inputs=L5,
         units=84,
         kernel_initializer=kernel,
         activation='relu')
-    L7_fc = tf.layers.dense(inputs=L6_fc, units=10, kernel_initializer=kernel,)
-    Y_pred = tf.nn.softmax(L7_fc)
-    loss = tf.losses.softmax_cross_entropy(y, L7_fc)
-    Train_op = tf.train.AdamOptimizer().minimize(loss)
-    acc = tf.reduce_mean(
+    L7 = tf.layers.dense(inputs=L6, units=10, kernel_initializer=kernel,)
+    Y_pred = tf.nn.softmax(L7)
+    loss = tf.losses.softmax_cross_entropy(y, L7)
+    Training = tf.train.AdamOptimizer().minimize(loss)
+    accuracy = tf.reduce_mean(
         tf.cast(
             tf.equal(
                 tf.argmax(
-                    L7_fc, axis=1), tf.argmax(
+                    L7, axis=1), tf.argmax(
                     y, axis=1)), "float32"))
-    return Y_pred, Train_op, loss, acc
+    return Y_pred, Training, loss, accuracy
