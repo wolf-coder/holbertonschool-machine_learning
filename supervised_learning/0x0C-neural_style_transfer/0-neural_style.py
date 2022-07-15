b0d455e41cc92550b41253668a032c7e7cd1edb8
wolf-coder <cuore.fakhri@gmail.com>
2022-07-15 05:06:21 +0100

    Initialize

--- supervised_learning/0x0C-neural_style_transfer/0-neural_style.py
+++ supervised_learning/0x0C-neural_style_transfer/0-neural_style.py
@@ -6,15 +6,24 @@ import tensorflow as tf
 
 class NST:
     """NST class"""
-    style_layers = ['block1_conv1', 'block2_conv1', 'block3_conv1', 'block4_conv1', 'block5_conv1']
+    style_layers = [
+        'block1_conv1',
+        'block2_conv1',
+        'block3_conv1',
+        'block4_conv1',
+        'block5_conv1']
     content_layer = 'block5_conv2'
 
     def __init__(self, style_image, content_image, alpha=1e4, beta=1):
         """init function"""
-        if (not isinstance(style_image, np.ndarray) or len(style_image.shape) != 3 or style_image.shape[2] != 3):
-            raise TypeError("style_image must be a numpy.ndarray with shape (h, w, 3)")
-        if (not isinstance(content_image, np.ndarray) or len(content_image.shape) != 3 or content_image.shape[2] != 3):
-            raise TypeError("content_image must be a numpy.ndarray with shape (h, w, 3)")
+        if (not isinstance(style_image, np.ndarray) or len(
+                style_image.shape) != 3 or style_image.shape[2] != 3):
+            raise TypeError(
+                "style_image must be a numpy.ndarray with shape (h, w, 3)")
+        if (not isinstance(content_image, np.ndarray) or len(
+                content_image.shape) != 3 or content_image.shape[2] != 3):
+            raise TypeError(
+                "content_image must be a numpy.ndarray with shape (h, w, 3)")
         if not isinstance(alpha, (int, float)) or alpha < 0:
             raise TypeError("alpha must be a non-negative number")
         if not isinstance(beta, (int, float)) or beta < 0:
@@ -31,8 +40,10 @@ class NST:
         """
             rescales an image
         """
-        if (not isinstance(image, np.ndarray) or len(image.shape) != 3 or image.shape[2] != 3):
-            raise TypeError("image must be a numpy.ndarray with shape (h, w, 3)")
+        if (not isinstance(image, np.ndarray) or len(
+                image.shape) != 3 or image.shape[2] != 3):
+            raise TypeError(
+                "image must be a numpy.ndarray with shape (h, w, 3)")
 
         h, w, _ = image.shape
         h_new = w_new = 512
