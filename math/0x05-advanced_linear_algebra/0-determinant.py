1529def5d21a852f851917c1c1fe208defe97a57
wolf-coder <cuore.fakhri@gmail.com>
2022-08-27 20:56:50 +0100

    fixed

--- math/0x05-advanced_linear_algebra/0-determinant.py
+++ math/0x05-advanced_linear_algebra/0-determinant.py
@@ -4,23 +4,6 @@ Matrix determinant
 """
 
 
-def matrix_shape(arg):
-    """
-    mehtod that calculates the shape of a matrix:
-    - You can assume all elements in the same dimension are
-        of the same type/shape.
-    - The shape should be returned as a list of integers.
-    """
-    shape = [len(arg)]
-    if len(arg) != 0:
-        while len(arg) != 0 and type(arg[0]) == list:
-            shape.append(len(arg[0]))
-            arg = arg[0]
-        return shape
-    else:
-        return [0]
-
-
 def Laplace(matrix):
     """
     Using Laplace Expansion method to get the determinant of a matrix.
@@ -51,31 +34,18 @@ matrix must be a square matrix.
         - The list [[]] represents a 0x0 matrix.
         - Returns: the determinant of matrix.
     """
-    """
-    if len(matrix) == 0 or\
-       any([(False if type(el) == list else True) for el in matrix]):
-        raise TypeError("matrix must be a list of lists")
 
-    if matrix == [[]]:  # [[]] is a 0x0 matrix => (det = 1)
-        return 1
-    shape = matrix_shape(matrix)
-    if shape[0] != shape[1]:
-        raise ValueError('matrix must be a square matrix')
-
-    if matrix_shape(matrix) == [1, 1]:  # 1x1 matrix
-        return matrix[0][0]
-    """
     if type(matrix) is not list or not matrix:
         raise TypeError('matrix must be a list of lists')
-    if matrix == [[]]:
+    if matrix == [[]]:  # 0x0 matrix
         return 1
-    m = len(matrix)
+    col = len(matrix)
     for row in matrix:
         if type(row) is not list:
             raise TypeError('matrix must be a list of lists')
-        if len(row) !=  m:
+        if len(row) != col:
             raise ValueError('matrix must be a square matrix')
-    if m == 1:
+    if col == 1:
         return matrix[0][0]
-    
+
     return Laplace(matrix)
