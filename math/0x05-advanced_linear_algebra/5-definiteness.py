aa7c85f1b95b7cf814c93b6366ae84376e510431
wolf-coder <cuore.fakhri@gmail.com>
2022-08-31 23:40:27 +0100

    d

--- math/0x05-advanced_linear_algebra/5-definiteness.py
+++ math/0x05-advanced_linear_algebra/5-definiteness.py
@@ -6,19 +6,7 @@ import numpy as np
 
 def definiteness(matrix):
     """
-    - Function that calculates the definiteness of a matrix:
-    - matrix is a numpy.ndarray of shape (n, n) whose definiteness
-        should be calculated.
-    - If matrix is not a numpy.ndarray, raise a TypeError with the message
-        matrix must be a numpy.ndarray.
-    - If matrix is not a valid matrix, return None.
-    - Return: the string:
-        * Positive definite,
-        * Positive semi-definite,
-        * Negative semi-definite,
-        * Negative definite, or Indefinite
-    - If matrix does not fit any of the above categories, return None
-    - You may import numpy as np
+    Function that calculates the definiteness of a matrix:
     """
     if not isinstance(matrix, np.ndarray):
         raise TypeError("matrix must be a numpy.ndarray")
@@ -27,7 +15,7 @@ def definiteness(matrix):
     if len(Shape) != 2 or (Shape[0] != Shape[1]):
         return None
     Transpose = matrix.transpose()
-    if not np.array_equal(matrix, Transpose):  # Not a valid matrix
+    if not np.array_equal(matrix, Transpose):
         return None
 
     EigenValues, _ = np.linalg.eig(matrix)
