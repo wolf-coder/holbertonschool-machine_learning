4347791e480fedaca5a8fe2a7b0f39de483bc354
wolf-coder <cuore.fakhri@gmail.com>
2022-08-31 23:25:55 +0100

    try this

--- math/0x05-advanced_linear_algebra/5-definiteness.py
+++ math/0x05-advanced_linear_algebra/5-definiteness.py
@@ -1,45 +1,41 @@
 #!/usr/bin/env python3
-""" definiteness of a matrix """
+""" 0x05-advanced_linear_algebra """
 
 import numpy as np
 
 
 def definiteness(matrix):
     """
-    - Function that calculates the definiteness of a matrix:
-    - matrix is a numpy.ndarray of shape (n, n) whose definiteness
-        should be calculated.
-    - If matrix is not a numpy.ndarray, raise a TypeError with the message
-        matrix must be a numpy.ndarray.
-    - If matrix is not a valid matrix, return None.
-    - Return: the string:
-        * Positive definite,
-        * Positive semi-definite,
-        * Negative semi-definite,
-        * Negative definite, or Indefinite
-    - If matrix does not fit any of the above categories, return None
-    - You may import numpy as np
+    calculates the definiteness of a matrix:
+    matrix is a numpy.ndarray of shape (n, n) whose definiteness should be
+        calculated
+    If matrix is not a numpy.ndarray, raise a TypeError with the message
+        matrix must be a numpy.ndarray
+    If matrix is not a valid matrix, return None
+    Return: the string Positive definite, Positive semi-definite, Negative
+        semi-definite, Negative definite, or Indefinite if the matrix is
+        positive definite, positive semi-definite, negative semi-definite,
+        negative definite of indefinite, respectively
+    If matrix does not fit any of the above categories, return None
     """
+
     if not isinstance(matrix, np.ndarray):
         raise TypeError("matrix must be a numpy.ndarray")
-
-    Shape = matrix.shape
-    if len(Shape) != 2\
-       or (Shape[0] != Shape[1])\
-       or not np.array_equal(matrix, matrix.transpose()):  # Not a valid matrix
+    if len(matrix.shape) != 2 or matrix.shape[0] != matrix.shape[1]:
+        return None
+    transpose = np.transpose(matrix)
+    if not np.array_equal(transpose, matrix):
         return None
 
-    EigenValues, _ = np.linalg.eig(matrix)
-    Str = ""
-    if np.all(EigenValues >= 0):
-        Str = 'Positive definite'
-        if np.any(EigenValues == 0):
-            Str = 'Positive semi-definite'
-        return Str
-    if np.all(EigenValues <= 0):
-        Str = 'Negative definite'
-        if np.any(EigenValues == 0):
-            Str = 'Negative semi-definite'
-        return Str
+    w, _ = np.linalg.eig(matrix)
 
-    return 'Indefinite'
+    if all(w > 0):
+        return 'Positive definite'
+    elif all(w >= 0):
+        return 'Positive semi-definite'
+    elif all(w < 0):
+        return 'Negative definite'
+    elif all(w <= 0):
+        return 'Negative semi-definite'
+    else:
+        return 'Indefinixste'
