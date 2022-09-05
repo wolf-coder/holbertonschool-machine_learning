11211b13c3a43f0c178c42bca0aa17458f0238d9
wolf-coder <cuore.fakhri@gmail.com>
2022-09-05 23:24:57 +0100

    try

--- math/0x06-multivariate_prob/0-mean_cov.py
+++ math/0x06-multivariate_prob/0-mean_cov.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 """
+Multivariate_Normal distribution parameters:
+covariance, mean
 """
 
 
@@ -13,20 +15,7 @@ def Cov(V0, V1, M0, M1, n):
 
 def mean_cov(X):
     """
-    * Function that calculates the mean and covariance of a data set:
-        * X is a numpy.ndarray of shape (n, d) containing the data set:
-            * n is the number of data points
-            * d is the number of dimensions in each data point
-            * If X is not a 2D numpy.ndarray,
-raise a TypeError with the message X must be a 2D numpy.ndarray
-            * If n is less than 2, raise a ValueError with the message
-X must contain multiple data points
-        * Returns: mean, cov:
-            * mean is a numpy.ndarray of shape (1, d)
-containing the mean of the data set
-            * cov is a numpy.ndarray of shape (d, d)
-containing the covariance matrix of the data set
-        * You are not allowed to use the function numpy.cov.
+    Function that calculates the mean and covariance of a data set:
     """
 
     Shape = X.shape
