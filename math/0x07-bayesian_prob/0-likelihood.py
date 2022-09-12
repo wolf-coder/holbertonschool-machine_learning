b9bafe1f3ecc806c8200a93d1f00b001f7867a93
wolf-coder <cuore.fakhri@gmail.com>
2022-09-12 12:01:29 +0100

    try 5

--- math/0x07-bayesian_prob/0-likelihood.py
+++ math/0x07-bayesian_prob/0-likelihood.py
@@ -34,10 +34,11 @@ of obtaining the data, x and n, for each probability in P, respectively
     if not isinstance(n, int) or n <= 0:
         raise ValueError('n must be a positive integer')
     if not isinstance(x, int) or x < 0:
-        raise ValueError('x must be an integer that is greater than or equal to 0')
+        raise ValueError(
+            'x must be an integer that is greater than or equal to 0')
     if x > n:
         raise ValueError('x cannot be greater than n')
-    
+
     if not isinstance(P, np.ndarray) or P.ndim != 1:
         raise TypeError('P must be a 1D numpy.ndarray')
     if not ((P <= 1).all() and (P >= 0).all()):
