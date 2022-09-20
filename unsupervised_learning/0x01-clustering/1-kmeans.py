73b0c45af468d5d77845862fe6638cc8cd518d21
wolf-coder <cuore.fakhri@gmail.com>
2022-09-20 23:04:42 +0100

    try 2

--- unsupervised_learning/0x01-clustering/1-kmeans.py
+++ unsupervised_learning/0x01-clustering/1-kmeans.py
@@ -3,19 +3,6 @@
 Performs K-means on a dataset.
 """
 import numpy as np
-def initialize(X, k):
-    """
-    Function that initializes cluster centroids for K-means:
-    """
-    if type(X) is not np.ndarray or len(X.shape) != 2:
-        return None
-    if type(k) is not int or k <= 0 or k > X.shape[0]:
-        return None
-    n, d = X.shape
-    low = np.amin(X, axis=0)
-    high = np.amax(X, axis=0)
-    centroids = np.random.uniform(low=low, high=high, size=(k, d))
-    return centroids
 
 
 def Get_clss(X, C):
@@ -40,7 +27,6 @@ Function that performs K-means on a dataset:
     You may use at most 2 loops
     Returns: C, clss, or None, None on failure
         C is a numpy.ndarray of shape (k, d) containing the centroid means for each cluster
-        
     """
 
     if type(X) is not np.ndarray or X.ndim != 2:
@@ -51,23 +37,23 @@ Function that performs K-means on a dataset:
        or int(iterations) != iterations\
            or iterations < 1:
         return None, None
-    
+    n, d = X.shape
     mins = np.min(X, axis=0)
     maxs = np.max(X, axis=0)
 
-    C = initialize(X, k)
-    nC = C.copy()
+    Centroids = np.random.uniform(mins, maxs, size=(k, d))
+    C_copy = Centroids.copy()
     for _ in range(iterations):
-        clss = Get_clss(X, C)
+        clss = Get_clss(X, Centroids)
         for i in range(k):
             indices = np.argwhere(clss == i).reshape(-1)
             if X[indices].shape[0] > 0: #  No point associated to cluster i
-                nC[i] = np.mean(X[indices], axis=0)
+                C_copy[i] = np.mean(X[indices], axis=0)
             else:
-                nC[i] = np.random.uniform(mins, maxs)
-        if np.array_equal(nC, C): # No changement  
+                C_copy[i] = np.random.uniform(mins, maxs)
+        if np.array_equal(C_copy, Centroids): # No changement  
             break
-        C = nC.copy()
-    clss = Get_clss(X, C)
-    return C, clss
+        Centroids = C_copy.copy()
+    clss = Get_clss(X, Centroids)
+    return Centroids, clss
     
