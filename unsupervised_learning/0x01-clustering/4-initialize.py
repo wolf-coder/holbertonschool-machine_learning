739095da56b93b093234feefd53bf1c69f546846
wolf-coder <cuore.fakhri@gmail.com>
2022-09-22 16:58:14 +0100

    try 2

--- unsupervised_learning/0x01-clustering/4-initialize.py
+++ unsupervised_learning/0x01-clustering/4-initialize.py
@@ -14,19 +14,21 @@ def initialize(X, k):
     k is a positive integer containing the number of clusters
     You are not allowed to use any loops
     Returns: `pi, m, S`, or `None, None, None` on failure
-        pi is a numpy.ndarray of shape (k,) containing the priors for each cluster, initialized evenly
-        m is a numpy.ndarray of shape (k, d) containing the centroid means for each cluster, initialized with K-means
-        S is a numpy.ndarray of shape (k, d, d) containing the covariance matrices for each cluster, initialized as identity matrices
-
+        pi is a numpy.ndarray of shape (k,) containing the
+priors for each cluster, initialized evenly
+        m is a numpy.ndarray of shape (k, d) containing the centroid means
+for each cluster, initialized with K-means
+        S is a numpy.ndarray of shape (k, d, d) containing the covarianc
+matrices for each cluster, initialized as identity matrices
     """
-    if type(X) is not np.ndarray or X.ndim != 2:
+    if not isinstance(X, np.ndarray) or X.ndim != 2:
         return None, None, None
-    if type(k) is not int or k < 1:
+    if not isinstance(k, int) or k < 1:
         return None, None, None
 
     _,  d = X.shape
+
     pi = np.full(k, 1/k)
     m, _ = kmeans(X, k)
-      
     S = np.tile(np.eye(d), (k, 1, 1))
-    return pi, m, S    
+    return pi, m, S
