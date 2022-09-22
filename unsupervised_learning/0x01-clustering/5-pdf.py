c3bb46e5cbb6f7ad73227945454f6c12b9f04ff2
wolf-coder <cuore.fakhri@gmail.com>
2022-09-22 18:22:08 +0100

    Final

--- unsupervised_learning/0x01-clustering/5-pdf.py
+++ unsupervised_learning/0x01-clustering/5-pdf.py
@@ -4,20 +4,27 @@ Calculates the probability density function of a Gaussian distribution.
 """
 import numpy as np
 
+
 def pdf(X, m, S):
     """
-    function that calculates the probability density function of a Gaussian distribution:
+    function that calculates the probability density function
+of a Gaussian distribution:
 
-    X is a numpy.ndarray of shape (n, d) containing the data points whose PDF should be evaluated
-    m is a numpy.ndarray of shape (d,) containing the mean of the distribution
-    S is a numpy.ndarray of shape (d, d) containing the covariance of the distribution
+    X is a numpy.ndarray of shape (n, d) containing the data
+points whose PDF should be evaluated
+    m is a numpy.ndarray of shape (d,) containing the mean of
+the distribution
+    S is a numpy.ndarray of shape (d, d) containing the
+covariance of the distribution
     You are not allowed to use any loops
-    You are not allowed to use the function numpy.diag or the method numpy.ndarray.diagonal
+    You are not allowed to use the function numpy.diag or the
+method numpy.ndarray.diagonal
     Returns: P, or None on failure
-        P is a numpy.ndarray of shape (n,) containing the PDF values for each data point
+        P is a numpy.ndarray of shape (n,) containing the PDF
+values for each data point
 
     """
-    
+
     if not isinstance(X, np.ndarray) or X.ndim != 2:
         return None
     d = X.shape[1]
@@ -32,4 +39,6 @@ def pdf(X, m, S):
     num = np.exp(exp)
     det = np.linalg.det(S)
     pdf = num / np.sqrt(((2 * np.pi) ** d) * det)
-    return  np.maximum(pdf, 1e-300) # All values in P should have a minimum value of 1e-300
+
+    # All values in P should have a minimum value of 1e-300
+    return np.maximum(pdf, 1e-300)
