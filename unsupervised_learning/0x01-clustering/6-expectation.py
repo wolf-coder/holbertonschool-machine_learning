6cfe9250775d9fdd9d4319684f54571025e2ae07
wolf-coder <cuore.fakhri@gmail.com>
2022-09-24 20:57:47 +0100

    try 0

---
+++ unsupervised_learning/0x01-clustering/6-expectation.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+"""
+Calculating the expectation step in the EM algorithm for a GMM.
+"""
+import numpy as np
+pdf = __import__('5-pdf').pdf
+
+
+def expectation(X, pi, m, S):
+    """
+Function that calculates the expectation step in the EM algorithm for a GMM:
+
+    X is a numpy.ndarray of shape (n, d) containing the data set
+    pi is a numpy.ndarray of shape (k,) containing the priors for each cluster
+    m is a numpy.ndarray of shape (k, d) containing the centroid means for each cluster
+    S is a numpy.ndarray of shape (k, d, d) containing the covariance matrices for each cluster
+    You may use at most 1 loop
+    Returns: g, l, or None, None on failure
+        g is a numpy.ndarray of shape (k, n) containing the posterior probabilities
+for each data point in each cluster
+        l is the total log likelihood
+    """
+
+    n = X.shape[0] # Number of data points
+    k = pi.shape[0] #  Pi contains the priors for each cluster, initialized evenly
+    Posteriors = np.zeros((k, n))  # Initialization
+
+    for i in range(k):
+        Posteriors[i] = pi[i] * pdf(X, m[i], S[i])
+    Log = np.sum(Posteriors, axis=0, keepdims=True) # l is the total log likelihood
+    Posteriors = Posteriors / Log
+    Log = np.sum(np.log(Log))
+    return Posteriors, Log
