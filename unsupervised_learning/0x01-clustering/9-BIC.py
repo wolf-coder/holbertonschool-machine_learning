201b0f6b761222f1bc6938764841e07f9a9476ec
wolf-coder <cuore.fakhri@gmail.com>
2022-09-27 00:11:08 +0100

    try 2

--- unsupervised_learning/0x01-clustering/9-BIC.py
+++ unsupervised_learning/0x01-clustering/9-BIC.py
@@ -2,7 +2,7 @@
 """
 Finding the best number of clusters for a GMM using the Bayesian Information Criterion.
 """
-import matplotlib.pyplot as plt
+import numpy as np
 expectation_maximization = __import__('8-EM').expectation_maximization
 
 def BIC(X, kmin=1, kmax=None, iterations=1000, tol=1e-5, verbose=False):
