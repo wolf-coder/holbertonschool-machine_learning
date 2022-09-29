936d5db6513c8b41c4059b46ffdb00bd25aa0376
wolf-coder <cuore.fakhri@gmail.com>
2022-09-30 00:34:36 +0100

    try 1

---
+++ unsupervised_learning/0x02-hmm/1-regular.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+"""
+Determination of the steady state probabilities of a regular markov chain.
+"""
+import numpy as np
+
+def regular(P):
+    """
+Function that determines the steady state probabilities of a regular markov chain:
+
+    P is a is a square 2D numpy.ndarray of shape (n, n) representing the transition matrix
+        P[i, j] is the probability of transitioning from state i to state j
+        n is the number of states in the markov chain
+    Returns: a numpy.ndarray of shape (1, n) containing the steady state probabilities, or None on failure
+    """
+    
+    n= P.shape[0]
+    d=P.shape[1]
+    if type(P) is not np.ndarray or P.ndim!=2 or n!= d :
+        return None 
+    if not np.allclose(np.sum(P, axis=1), 1):
+        return None
+    n = P.shape[0]
+    q = (P-np.eye(n))
+    Ones = np.ones(n)
+    q = np.c_[q,Ones]
+    
+    QxQ = np.dot(q, q.T)
+    try: # Can't solve it as QxQ is singular or not squarred
+        steady_state_prob = np.linalg.solve(QxQ,Ones)
+    except np.linalg.LinAlgError:
+        return None
+    return steady_state_prob
