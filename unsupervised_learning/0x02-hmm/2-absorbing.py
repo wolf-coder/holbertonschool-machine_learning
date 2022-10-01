14a698bd9d675df2dad9d898d7a82e7a2e9588b2
wolf-coder <cuore.fakhri@gmail.com>
2022-10-01 15:12:06 +0100

    try 0

---
+++ unsupervised_learning/0x02-hmm/2-absorbing.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python3
+"""
+Determine if a markov chain is absorbing.
+"""
+import numpy as np
+def absorbing(P):
+    """
+function that determines if a markov chain is absorbing:p
+
+    P is a is a square 2D numpy.ndarray of shape (n, n) representing the STANDARD TRANSITION MATRIX
+        P[i, j] is the probability of transitioning from state i to state j
+        n is the number of states in the markov chain
+    Returns: True if it is absorbing, or False on failure
+    """
+    if type(P) is not np.ndarray:
+        return False
+    if P.ndim != 2 or P.shape[0] != P.shape[1]:
+        return False
+    
+    if not np.allclose(np.sum(P, axis=1), 1):  # Check Markov or not
+        return None
+
+    n = P.shape[0]
+
+    
+    Diag  = np.diag(P)  #  get the matrix diagonal.
+
+    if np.all(Diag != 1): #  None steady state.
+        return False
+
+    if np.all(Diag == 1): #  all Steady state.
+        return True
+    
+    """
+    Get the limiting transitioning matrix P_l
+    """
+    n = P.shape[0]
+    #  Get I
+    Diag = np.diag(P)
+    I_diag_len = np.count_nonzero(Diag == 1)
+    I = np.identity(I_diag_len)
+    
+    #  Get R
+    R = P[slice(I_diag_len,n),slice(I_diag_len)]
+    #  Get Q
+    Q = P[slice(I_diag_len,n),slice(I_diag_len,n)]
+    #  F = (I -Q).inverse
+    try:
+        F = np.linalg.inv(np.eye(Q.shape[0]) - Q)
+    except np.linalg.LinAlgError:
+        return False
+    #  F @ R
+    FR = F @ R
+    
+    return True
