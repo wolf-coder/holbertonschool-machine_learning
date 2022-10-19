0850cd7a9874627fc610f618e63ba850cb6496fe
wolf-coder <cuore.fakhri@gmail.com>
2022-10-19 22:43:03 +0100

    Final

--- unsupervised_learning/0x02-hmm/4-viterbi.py
+++ unsupervised_learning/0x02-hmm/4-viterbi.py
@@ -8,15 +8,21 @@ def viterbi(Observation, Emission, Transition, Initial):
     """
     Observation is a numpy.ndarray of shape (T,) that contains the index of the observation
         T is the number of observations
-    Emission is a numpy.ndarray of shape (N, M) containing the emission probability of a specific observation given a hidden state
-        Emission[i, j] is the probability of observing j given the hidden state i
+    Emission is a numpy.ndarray of shape (N, M) containing the
+emission probability of a specific observation given a hidden state
+        Emission[i, j] is the probability of observing j given
+the hidden state i
         N is the number of hidden states
         M is the number of all possible observations
-    Transition is a 2D numpy.ndarray of shape (N, N) containing the transition probabilities
-        Transition[i, j] is the probability of transitioning from the hidden state i to j
-    Initial a numpy.ndarray of shape (N, 1) containing the probability of starting in a particular hidden state
+    Transition is a 2D numpy.ndarray of shape (N, N) containing
+the transition probabilities
+        Transition[i, j] is the probability of transitioning
+from the hidden state i to j
+    Initial a numpy.ndarray of shape (N, 1) containing the
+probability of starting in a particular hidden state
     Returns: path, P, or None, None on failure
-        path is the a list of length T containing the most likely sequence of hidden states
+        path is the a list of length T containing the most
+likely sequence of hidden states
         P is the probability of obtaining the path sequence
     """
     T = Observation.shape[0]
@@ -25,17 +31,17 @@ def viterbi(Observation, Emission, Transition, Initial):
     try:
         V[:, 0] = Initial.T * Emission[:, Observation[0]] #  Initialisaton Step
         backpointer = np.zeros((N, T))
-        for t in range(1, T): # recursion step
+        for t in range(1, T): # Recursion step
             for s in range(N):
                 tr = Transition[:, s]
                 em = Emission[s, Observation[t]]
                 V[s, t] = np.max(V[:, t - 1] * tr * em)
                 backpointer[s, t] = np.argmax(V[:, t - 1] * tr * em)
-        bestPath = [np.argmax(V[:, T - 1])] #  termination step
+        bestPath = [np.argmax(V[:, T - 1])] #  Termination step
         for t in range(T - 1, 0, -1):
             bestPath.append(int(backpointer[bestPath[-1], t]))
         bestPath = bestPath[::-1]
-        bestPathProb = np.max(V[:, T - 1]) #  termination step
+        bestPathProb = np.max(V[:, T - 1]) #  Termination step
         return bestPath, bestPathProb
     except Exception:
         return None, None
