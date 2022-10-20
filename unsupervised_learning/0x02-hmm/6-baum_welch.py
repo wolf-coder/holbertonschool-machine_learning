1ffa7a365f24e30d782950416897fd825b1cbd45
wolf-coder <cuore.fakhri@gmail.com>
2022-10-20 17:19:11 +0100

    try 2

--- unsupervised_learning/0x02-hmm/6-baum_welch.py
+++ unsupervised_learning/0x02-hmm/6-baum_welch.py
@@ -2,8 +2,94 @@
 """
 Performing the Baum-Welch algorithm for a hidden markov model.
 """
-forward = __import__('3-forward').forward
-backward = __import__('5-backward').backward
+def backward(Observation, Emission, Transition, Initial):
+    """
+    def backward(Observation, Emission, Transition, Initial): that
+performs the backward algorithm for a hidden markov model:
+
+    Observation is a numpy.ndarray of shape (T,) that contains the
+index of the observation
+        T is the number of observations
+    Emission is a numpy.ndarray of shape (N, M) containing the
+emission probability of a specific observation given a hidden state
+        Emission[i, j] is the probability of observing j given the
+hidden state i
+        N is the number of hidden states
+        M is the number of all possible observations
+    Transition is a 2D numpy.ndarray of shape (N, N) containing the
+transition probabilities
+        Transition[i, j] is the probability of transitioning from the
+hidden state i to j
+    Initial a numpy.ndarray of shape (N, 1) containing the
+probability of starting in a particular hidden state
+    Returns: P, B, or None, None on failure
+        Pis the likelihood of the observations given the model
+        B is a numpy.ndarray of shape (N, T) containing the backward
+path probabilities
+            B[i, j] is the probability of generating the future
+observations from hidden state i at time j
+    """
+    try:
+        T = Observation.shape[0]
+        N = Transition.shape[0]
+
+        B = np.zeros((N, T))
+        B[:, T - 1] = np.ones((N))
+
+        for t in range(T - 2, -1, -1):
+            for j in range(N):
+                B[j, t] = (B[:, t + 1] * Emission[:, Observation[t + 1]]) @\
+                    (Transition[j, :])
+
+        Pis = np.sum(np.sum(Initial.T * Emission[:, Observation[0]] * B[:, 0]))
+        return Pis, B
+    except Exception:
+        return None, None
+
+
+def forward(Observation, Emission, Transition, Initial):
+
+    """
+    Function that performs the forward algorithm for a hidden markov model:
+
+    Observation is a numpy.ndarray of shape (T,) that contains
+the index of the observation
+        T is the number of observations
+    Emission is a numpy.ndarray of shape (N, M) containing the
+emission probability of a specific observation given a hidden state
+        Emission[i, j] is the probability of observing j given the
+hidden state i
+        N is the number of hidden states
+        M is the number of all possible observations
+    Transition is a 2D numpy.ndarray of shape (N, N) containing
+the transition probabilities
+        Transition[i, j] is the probability of transitioning
+from the hidden state i to j
+    Initial a numpy.ndarray of shape (N, 1) containing the probability
+of starting in a particular hidden state
+    Returns: P, F, or None, None on failure
+        P is the likelihood of the observations given the model
+        F is a numpy.ndarray of shape (N, T) containing the forward
+path probabilities
+        F[i, j] is the probability of being in hidden state i at time
+j given the previous observations
+    """
+
+    T = Observation.shape[0]
+    N = Transition.shape[0]
+    F = np.ones((N, T))  # create a probability matrix forward[N,T]
+    try:
+        F[:, 0] = Initial.T * Emission[:, Observation[0]]  # Initialisaton Step
+        for t in range(1, T):  # Recursion step.
+            for s in range(N):
+                tr = Transition[slice(None), s]
+                em = Emission[s, Observation[t]]
+                F[s, t] = np.sum(F[:, t - 1] * tr * em)
+        P = np.sum(F[:, T - 1])  # Termination Step.
+        return P, F
+    except Exception:
+        return None, None
+
 
 import numpy as np
 
