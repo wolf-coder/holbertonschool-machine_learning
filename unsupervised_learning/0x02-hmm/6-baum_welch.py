d808c8b92c1d110b34135427b398edf2afe16c50
wolf-coder <cuore.fakhri@gmail.com>
2022-10-20 17:22:52 +0100

    try 3

--- unsupervised_learning/0x02-hmm/6-baum_welch.py
+++ unsupervised_learning/0x02-hmm/6-baum_welch.py
@@ -4,30 +4,7 @@ Performing the Baum-Welch algorithm for a hidden markov model.
 """
 def backward(Observation, Emission, Transition, Initial):
     """
-    def backward(Observation, Emission, Transition, Initial): that
-performs the backward algorithm for a hidden markov model:
-
-    Observation is a numpy.ndarray of shape (T,) that contains the
-index of the observation
-        T is the number of observations
-    Emission is a numpy.ndarray of shape (N, M) containing the
-emission probability of a specific observation given a hidden state
-        Emission[i, j] is the probability of observing j given the
-hidden state i
-        N is the number of hidden states
-        M is the number of all possible observations
-    Transition is a 2D numpy.ndarray of shape (N, N) containing the
-transition probabilities
-        Transition[i, j] is the probability of transitioning from the
-hidden state i to j
-    Initial a numpy.ndarray of shape (N, 1) containing the
-probability of starting in a particular hidden state
-    Returns: P, B, or None, None on failure
-        Pis the likelihood of the observations given the model
-        B is a numpy.ndarray of shape (N, T) containing the backward
-path probabilities
-            B[i, j] is the probability of generating the future
-observations from hidden state i at time j
+    performs the backward algorithm for a hidden markov model
     """
     try:
         T = Observation.shape[0]
@@ -46,33 +23,9 @@ observations from hidden state i at time j
     except Exception:
         return None, None
 
-
 def forward(Observation, Emission, Transition, Initial):
-
     """
-    Function that performs the forward algorithm for a hidden markov model:
-
-    Observation is a numpy.ndarray of shape (T,) that contains
-the index of the observation
-        T is the number of observations
-    Emission is a numpy.ndarray of shape (N, M) containing the
-emission probability of a specific observation given a hidden state
-        Emission[i, j] is the probability of observing j given the
-hidden state i
-        N is the number of hidden states
-        M is the number of all possible observations
-    Transition is a 2D numpy.ndarray of shape (N, N) containing
-the transition probabilities
-        Transition[i, j] is the probability of transitioning
-from the hidden state i to j
-    Initial a numpy.ndarray of shape (N, 1) containing the probability
-of starting in a particular hidden state
-    Returns: P, F, or None, None on failure
-        P is the likelihood of the observations given the model
-        F is a numpy.ndarray of shape (N, T) containing the forward
-path probabilities
-        F[i, j] is the probability of being in hidden state i at time
-j given the previous observations
+    Performing the forward algorithm for a hidden markov model.
     """
 
     T = Observation.shape[0]
