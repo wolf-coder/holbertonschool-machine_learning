038671dd085b99eb5daf9975340c477d4515dd8c
wolf-coder <cuore.fakhri@gmail.com>
2022-10-20 18:49:43 +0100

    try 4

--- unsupervised_learning/0x02-hmm/6-baum_welch.py
+++ unsupervised_learning/0x02-hmm/6-baum_welch.py
@@ -48,14 +48,6 @@ import numpy as np
 
 def baum_welch(Observations, Transition, Emission, Initial, iterations=1000):
     """
-    Observations is a numpy.ndarray of shape (T,) that contains the index of the observation
-        T is the number of observations
-    Transition is a numpy.ndarray of shape (M, M) that contains the initialized transition probabilities
-        M is the number of hidden states
-    Emission is a numpy.ndarray of shape (M, N) that contains the initialized emission probabilities
-        N is the number of output states
-    Initial is a numpy.ndarray of shape (M, 1) that contains the initialized starting probabilities
-    iterations is the number of times expectation-maximization should be performed
     Returns: the converged Transition, Emission, or None, None on failure
     """
     try:
@@ -89,4 +81,3 @@ def baum_welch(Observations, Transition, Emission, Initial, iterations=1000):
         return a, b
     except Exception as e:
         return None, None
-    return Transition, Emission
