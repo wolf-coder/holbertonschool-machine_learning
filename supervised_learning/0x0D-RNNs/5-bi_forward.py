607fb629f661b7d02ebc606ce40a300ace9f7aa5
wolf-coder <cuore.fakhri@gmail.com>
2022-10-23 02:54:13 +0100

    try 1

---
+++ supervised_learning/0x0D-RNNs/5-bi_forward.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+"""
+bidirectional RNN
+"""
+import numpy as np
+
+
+class BidirectionalCell:
+    """
+    bidirectional RNN
+    """
+
+    def __init__(self, i, h, o):
+        """ 
+        init
+        """
+        self.Whf = np.random.normal(0.0, 1.0, (i+h, h))
+        self.Whb = np.random.normal(0.0, 1.0, (i+h, h))
+        self.Wy = np.random.normal(0.0, 1.0, (2*h, o))
+        self.bhf = np.zeros((1, h))
+        self.bhb = np.zeros((1, h))
+        self.by = np.zeros((1, o))
+
+    def forward(self, h_prev, x_t):
+        """
+        forward
+        """
+        x = np.concatenate((h_prev, x_t), axis=1)
+        h_next = np.tanh(np.matmul(x, self.Whf) + self.bhf)
+        return h_next
