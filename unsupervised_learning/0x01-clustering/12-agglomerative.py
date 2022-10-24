4cd509349137763c6ff6515574ac94b5a0840804
wolf-coder <cuore.fakhri@gmail.com>
2022-10-24 03:24:25 +0100

    uuu

--- unsupervised_learning/0x01-clustering/12-agglomerative.py
+++ unsupervised_learning/0x01-clustering/12-agglomerative.py
@@ -1,14 +1,24 @@
 #!/usr/bin/env python3
 """
-12-agglomerative.py
+Performing agglomerative clustering on a dataset
 """
-import scipy.cluster.hierarchy
 import matplotlib.pyplot as plt
+import scipy.cluster.hierarchy
 
 
 def agglomerative(X, dist):
     """
-    function that performs agglomerative clustering on a dataset
+    
+    X is a numpy.ndarray of shape (n, d) containing the dataset
+    dist is the maximum cophenetic distance for all clusters
+    Performs agglomerative clustering with Ward linkage
+    Displays the dendrogram with each cluster displayed in a
+different color
+    The only imports you are allowed to use are:
+        import scipy.cluster.hierarchy
+        import matplotlib.pyplot as plt
+    Returns: clss, a numpy.ndarray of shape (n,) containing
+the cluster indices for each data point
     """
 
     H = scipy.cluster.hierarchy
