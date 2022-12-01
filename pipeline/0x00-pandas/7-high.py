fc39d69e26215b9839da0c362944318ec0fe2e13
wolf-coder <cuore.fakhri@gmail.com>
2022-12-01 21:15:33 +0100

    Final

---
+++ pipeline/0x00-pandas/7-high.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+import pandas as pd
+from_file = __import__('2-from_file').from_file
+
+df = from_file('coinbaseUSD_1-min_data_2014-12-01_to_2019-01-09.csv',
+               ',')
+"""
+Sorting the pd.DataFrame by the High price in descending order.
+"""
+df = df.sort_values(by="High", ascending=False)
+
+print(df.head())
