9fae552b813dfeb6a912bebdb3ac4e30204e113b
wolf-coder <cuore.fakhri@gmail.com>
2022-12-01 17:07:51 +0100

    Final

---
+++ pipeline/0x00-pandas/2-from_file.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+Loading data from a file as a pd.DataFrame.
+"""
+import pandas as pd
+
+
+def from_file(filename, delimiter):
+    """
+    function that loads data from a file as a pd.DataFrame:
+        + filename is the file to load from
+        + delimiter is the column separator
+        + Returns: the loaded pd.DataFrame
+    """
+    return pd.read_csv(filename, delimiter=delimiter)
