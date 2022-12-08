c078aaa25baf8baac163c3ce08b6f58244c06182
wolf-coder <cuore.fakhri@gmail.com>
2022-12-08 18:13:16 +0100

    Try

--- pipeline/0x01-apis/1-sentience.py
+++ pipeline/0x01-apis/1-sentience.py
@@ -13,19 +13,18 @@ def sentientPlanets():
     """
     Sentient_planets = []
     url = "https://swapi-api.hbtn.io/api/species/"
-    a = 1
     while (url):
         Request = RE.get(url)
         DATA = Request.json()
         for result in DATA['results']:
-            if result['designation'] == 'sentient':
+            if result['designation'] == 'sentient' or\
+               result["designation"] == 'sentient':
                 #breakpoint()
                 if result['homeworld']:
                     Home_world = RE.get(result['homeworld']).json()['name']
                 else:
                     Home_world = "unknown"
-                Sentient_planets.append(Home_world)
+                if Home_world not in Sentient_planets:
+                    Sentient_planets.append(Home_world)
         url = DATA['next']
-        a+=1
-        print(a)
     return Sentient_planets
