d76853216a224dd8fc595c9281e46abca646eb1c
wolf-coder <cuore.fakhri@gmail.com>
2022-12-08 21:05:35 +0100

    check

--- pipeline/0x01-apis/4-rocket_frequency.py
+++ pipeline/0x01-apis/4-rocket_frequency.py
@@ -13,10 +13,10 @@ import time
 
 
 if __name__ == '__main__':
-    r = requests.get("https://api.spacexdata.com/v4/launches/")
+    Response = requests.get("https://api.spacexdata.com/v4/launches/")
     rockets = {}
-    r_get = r.json()
-    for launch in r_get:
+    DATA = Response.json()
+    for launch in DATA:
         rocket = launch["rocket"]
         rocket_url = "https://api.spacexdata.com/v4/rockets/" + rocket
         r_rocket = requests.get(rocket_url)
@@ -26,7 +26,7 @@ if __name__ == '__main__':
             rockets[rocket_name] = rockets[rocket_name] + 1
         else:
             rockets[rocket_name] = 1
-    sorting = sorted(rockets.items(), key=lambda x: x[0])
-    sorting = sorted(sorting, key=lambda x: x[1], reverse=True)
+    Sorted = sorted(rockets.items(), key=lambda x: x[0])
+    Sorted = sorted(Sorted, key=lambda x: x[1], reverse=True)
     for i in sorting:
         print("{}: {}".format(i[0], i[1]))
