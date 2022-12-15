d68c8280609d4cc52e807dc6c64c5b91e31f6bea
wolf-coder <cuore.fakhri@gmail.com>
2022-12-15 20:26:12 +0100

    check

---
+++ pipeline/0x02-databases/18-valid_email.sql
@@ -0,0 +1,11 @@
+-- an SQL script that creates a trigger that resets the attribute valid_email only when the email has been changed.
+
+DELIMITER $$
+CREATE TRIGGER email_check BEFORE UPDATE ON users
+FOR EACH ROW
+BEGIN
+	IF STRCMP(OLD.email, NEW.email) <> 0 THEN
+	   SET NEW.valid_email = 0;
+	END IF;	
+END$$
+DELIMITER ;
