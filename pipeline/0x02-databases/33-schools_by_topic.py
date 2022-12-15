e20e23a90f06cb210df9fdd32cf1b4f7bf421f5b
wolf-coder <cuore.fakhri@gmail.com>
2022-12-16 00:06:06 +0100

    chek

--- pipeline/0x02-databases/33-schools_by_topic.py
+++ pipeline/0x02-databases/33-schools_by_topic.py
@@ -4,7 +4,8 @@
 
 def schools_by_topic(mongo_collection, topic):
     """
-    - Write a Python function that returns the list of school having a specific topic:
+    - Write a Python function that returns the list of school having
+a specific topic:
       + Prototype: def schools_by_topic(mongo_collection, topic):
       + mongo_collection will be the pymongo collection object
       + topic (string) will be topic searched
