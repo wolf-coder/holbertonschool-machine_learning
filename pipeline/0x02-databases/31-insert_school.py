25dcda4861df4854e1296149c0d6260a497d9180
wolf-coder <cuore.fakhri@gmail.com>
2022-12-15 23:43:52 +0100

    chekc

--- pipeline/0x02-databases/31-insert_school.py
+++ pipeline/0x02-databases/31-insert_school.py
@@ -1,14 +1,19 @@
 #!/usr/bin/env python3
-'''inserts a new document in a collection based on kwargs'''
+"""
+inserts a new document in a collection based on kwargs
+"""
 
 
 def insert_school(mongo_collection, **kwargs):
     """
-    - Write a Python function that inserts a new document in
-a collection based on kwargs:
-      + Prototype: def insert_school(mongo_collection, **kwargs):
-      + mongo_collection will be the pymongo collection object
-      + Returns the new _id
+    - Python function that changes all topics of a school
+document based on the name:
+    + Prototype: def update_topics(mongo_collection, name, topics):
+    + mongo_collection will be the pymongo collection object
+    + name (string) will be the school name to update
+    + topics (list of strings) will be the list of topics
+approached in the school
+    + Returns the new _id
     """
 
     return mongo_collection.insert_one(kwargs).inserted_id
