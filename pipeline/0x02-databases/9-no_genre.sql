e98c5f13d87f1e447738bf979ed3081d4ee123f0
wolf-coder <cuore.fakhri@gmail.com>
2022-12-15 18:50:45 +0100

    Left join

--- pipeline/0x02-databases/9-no_genre.sql
+++ pipeline/0x02-databases/9-no_genre.sql
@@ -1,3 +1,4 @@
--- 9. No genre
-SELECT tv_shows.title, tv_show_genres.genre_id FROM tv_shows LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id WHERE tv_shows.id = tv_show_genres.genre_id IS NULL ORDER BY title, genre_id;
-
+-- No genre: Left join
+SELECT tv_shows.title, tv_show_genres.genre_id FROM tv_shows
+LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+WHERE tv_shows.id = tv_show_genres.genre_id IS NULL ORDER BY title, genre_id;
