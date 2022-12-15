67756cebe1dbe2e61d2c1cbeeb2a8517c3416179
wolf-coder <cuore.fakhri@gmail.com>
2022-12-16 00:15:11 +0100

    check

--- pipeline/0x02-databases/34-log_stats.py
+++ pipeline/0x02-databases/34-log_stats.py
@@ -1,34 +1,18 @@
-#!/usr/bin/env python3
-"""
-Python script that provides some stats about Nginx logs stored in MongoDB:
-  + Database: logs
-  + Collection: nginx
-  + Display (same as the example):
-  + first line: x logs where x is the number of documents in this collection
-  + second line: Methods:
-  + 5 lines with the number of documents with
-the method = ["GET", "POST", "PUT", "PATCH", "DELETE"] in this order
-(see example below - warning: it’s a tabulation before each line)
-  + one line with the number of documents with:
-  + method=GET
-  + path=/status
-"""
+"""script that provides some stats about Nginx logs stored in MongoDB"""
 from pymongo import MongoClient
 
 
 if __name__ == "__main__":
+    """ script that provides some stats abfdjjjjjjjjjjjjjjjjjout Nginx logs stored in MongoDB"""
     client = MongoClient('mongodb://127.0.0.1:27017')
-    school = client.logs.nginx
-    print('{} logs'.format(school.count_documents({})))
+    logs = client.logs.nginx
+    counts = logs.count_documents({})
+    print("{} logs".format(counts))
+    print("Methods:")
     methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
-    print('Methods:')
     for method in methods:
-        print('\tmethod {}: {}'.format(
-            method,
-            school.count_documents({'method': method})
-        ))
-    print('{} status check'.format(
-        school.count_documents(
-            {'method': 'GET', 'path': '/status'}
-        )
-    ))
+        nmethod = logs.count_documents({"method": method})
+        print("\tmethod {}: {}".format(method, nmethod))
+    filter_path = {"method": "GET", "path": "/status"}
+    npath = logs.count_documents(filter_path)
+    print("{} status check".format(npath))
