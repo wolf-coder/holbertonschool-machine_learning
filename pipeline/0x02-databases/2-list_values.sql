0841053bb67866bf2fb540a09e43f0becbc6faed
wolf-coder <cuore.fakhri@gmail.com>
2022-12-15 15:24:40 +0100

    check

---
+++ pipeline/0x02-databases/2-list_values.sql
@@ -0,0 +1,4 @@
+-- Script that lists all rows of the table first_table in your MySQL server.
+--  + All fields should be printed
+--  + The database name will be passed as an argument of the mysql command
+SELECT * FROM first_table;
