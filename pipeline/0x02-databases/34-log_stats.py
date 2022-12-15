ee073cffc0b82f403b77e2dd332e4cf2670f32fe
wolf-coder <cuore.fakhri@gmail.com>
2022-12-16 00:19:02 +0100

    check

--- pipeline/0x02-databases/34-log_stats.py
+++ pipeline/0x02-databases/34-log_stats.py
@@ -1,19 +1,19 @@
 #!/usr/bin/env python3
-"""script that provides some stats about Nginx logs stored in MongoDB"""
+'''34. Log stats'''
 from pymongo import MongoClient
-
-
 if __name__ == "__main__":
-    """ script that provides some stats abfdjjjjjjjjjjjjjjjjjout Nginx logs stored in MongoDB"""
     client = MongoClient('mongodb://127.0.0.1:27017')
-    logs = client.logs.nginx
-    counts = logs.count_documents({})
-    print("{} logs".format(counts))
-    print("Methods:")
-    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+    school = client.logs.nginx
+    print('{} logs'.format(school.count_documents({})))
+    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]  
+    print('Methods:')
     for method in methods:
-        nmethod = logs.count_documents({"method": method})
-        print("\tmethod {}: {}".format(method, nmethod))
-    filter_path = {"method": "GET", "path": "/status"}
-    npath = logs.count_documents(filter_path)
-    print("{} status check".format(npath))
+        print('\tmethod {}: {}'.format(
+            method,
+            school.count_documents({'method': method})
+        ))
+    print('{} status check'.format(
+        school.count_documents(
+            {'method': 'GET', 'path': '/status'}
+        )
+    ))
