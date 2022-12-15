c2cd01a7f8803ae2d43e5987229b471c1b40573d
wolf-coder <cuore.fakhri@gmail.com>
2022-12-15 20:19:22 +0100

    check

--- pipeline/0x02-databases/14-country_users.sql
+++ pipeline/0x02-databases/14-country_users.sql
@@ -1,4 +1,4 @@
---Write a SQL script that creates a table users following these requirements:
+-- Write a SQL script that creates a table users following these requirements:
 --  + With these attributes:
 --  + id, integer, never null, auto increment and primary key
 --  + email, string (255 characters), never null and unique
@@ -13,4 +13,4 @@ CREATE TABLE IF NOT EXISTS users (
     name VARCHAR(256),
     country ENUM('US', 'CO', 'TN') DEFAULT 'US',
     PRIMARY KEY (id)
-)
+);
