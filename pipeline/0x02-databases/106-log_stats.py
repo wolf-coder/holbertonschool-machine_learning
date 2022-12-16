ada1529423688899306839fe8ca5759c488cd092
wolf-coder <cuore.fakhri@gmail.com>
2022-12-16 16:39:27 +0100

    check

--- pipeline/0x02-databases/106-log_stats.py
+++ pipeline/0x02-databases/106-log_stats.py
@@ -1,38 +1,30 @@
 #!/usr/bin/env python3
-"""
-- Improve 34-log_stats.py by adding the top 10 of the most present IPs in the collection nginx of the database logs:
-  + The IPs top must be sorted (like the example below)
-"""  
+
 from pymongo import MongoClient
 
 
 if __name__ == "__main__":
-    """ providingstats about Nginx logs stored in MongoDB"""
     client = MongoClient('mongodb://127.0.0.1:27017')
-    logs = client.logs.nginx
-    num_doc = logs.count_documents({})
-    print("{} logs".format(num_doc))
-    print("Methods:")
+    school = client.logs.nginx
+    print('{} logs'.format(school.count_documents({})))
     methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+    print('Methods:')
     for method in methods:
-        nmethod = logs.count_documents({"method": method})
-        print("\tmethod {}: {}".format(method, nmethod))
-    filter_path = {"method": "GET", "path": "/status"}
-    npath = logs.count_documents(filter_path)
-    print("{} status check".format(npath))
+        print('\tmethod {}: {}'.format(
+            method,
+            school.count_documents({'method': method})
+        ))
+    print('{} status check'.format(
+        school.count_documents(
+            {'method': 'GET', 'path': '/status'}
+        )
+    ))
+    pipeline = [
+        {"$sortByCount": '$ip'},
+        {"$limit": 10},
+        {"$sort": {"ip": -1}},
+    ]
+    agg = school.aggregate(pipeline=pipeline)
     print("IPs:")
-    pipeline = [{"$group": {"_id": "$ip", "count": {"$sum": 1}}}]
-    ips = logs.aggregate(pipeline)
-    ips_list = []
-    for ip in ips:
-        ips_list.append(ip)
-    sorted_ips = sorted(ips_list, key=lambda i: i["count"], reverse=True)
-    i = 0
-    limit = 10
-    if len(sorted_ips) < 10:
-        limit = len(sorted_ips)
-    while i < limit:
-        ip = sorted_ips[i]["_id"]
-        count = sorted_ips[i]["count"]
-        print("\t{}: {}".format(ip, count))
-        i += 1
+    for ip in agg:
+        print('\t{}: {}'.format(ip['_id'], ip['count']))
