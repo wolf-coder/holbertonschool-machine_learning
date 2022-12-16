a4387e3edac9bf81a9a6c39254875519273b1bd2
wolf-coder <cuore.fakhri@gmail.com>
2022-12-16 16:40:11 +0100

    chk

--- pipeline/0x02-databases/106-log_stats.py
+++ pipeline/0x02-databases/106-log_stats.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python3
-
+"""
+- Improve 34-log_stats.py by adding the top 10 of the most present IPs in the collection nginx of the database logs:
+  + The IPs top must be sorted (like the example below)
+"""
 from pymongo import MongoClient
 
 
