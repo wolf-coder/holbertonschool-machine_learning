9a4c32b86639ca987c3751b2a6dc90d1df92359e
wolf-coder <cuore.fakhri@gmail.com>
2023-01-26 16:05:25 +0100

    Function added: rotate_image

---
+++ pipeline/0x04-data_augmentation/2-rotate.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""
+Creating a function  that flips an image
+"""
+import tensorflow as tf
+
+
+def flip_image(image):
+    """image: tf.Tensor"""
+    return tf.image.rot90(image)
