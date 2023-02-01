9d4a25f09e1f7b4f98156b8276452a8a39f9e367
wolf-coder <cuore.fakhri@gmail.com>
2023-02-01 19:16:43 +0100

    Adding function: policy gradient

--- reinforcement_learning/0x03-policy_gradients/policy_gradient.py
+++ reinforcement_learning/0x03-policy_gradients/policy_gradient.py
@@ -11,3 +11,16 @@ def policy(matrix, weight):
     """
     policy = np.exp(matrix.dot(weight)) / np.sum(np.exp(matrix.dot(weight)))
     return policy
+
+
+def policy_gradient(state, weight):
+    """
+    Computes Monte-Carlo policy
+    """
+    p = policy(state, weight)
+    action = np.random.choice(len(p[0]), p=p[0])
+    p_r = p.reshape(-1, 1)
+    d_softmax = (np.diagflat(p_r) - np.dot(p_r, p_r.T))[action, :]
+    d_log = d_softmax / p[0, action]
+    grad = state.T.dot(d_log[None, :])
+    return action, grad
