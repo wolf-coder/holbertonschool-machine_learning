9fdd4122cc3ce132ddbfaf17d37fc44ad8c191cd
wolf-coder <cuore.fakhri@gmail.com>
2023-02-24 16:57:17 +0100

    SDP

---
+++ supervised_learning/0x11-attention/5-sdp_attention.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""
+SCALED DOT PRODUCT
+"""
+import tensorflow as tf
+
+
+def sdp_attention(Q, K, V, mask=None):
+    """
+    scaled dot product
+    """
+    sqr = tf.math.sqrt(
+        tf.cast(tf.shape(K)[-1], tf.float32))
+    scld = tf.matmul(Q, K, transpose_b=True) / sqr
+    if mask:
+        scld += (mask * -1e9)
+    sfmx = tf.nn.softmax(scld, axis=-1)
+    return tf.matmul(sfmx, V), sfmx
