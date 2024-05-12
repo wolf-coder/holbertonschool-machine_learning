a446a8a71cd0c6156cd183696a8765372d1f9df1
Fakhri Baklouti <cuore.fakhri@gmail.com>
2024-05-12 22:19:50 +0100

    skip

--- math/0x01-plotting/100-gradient.py
+++ math/0x01-plotting/100-gradient.py
@@ -1,20 +1,23 @@
-#!/usr/bin/env python3
-import matplotlib.pyplot as plt
 import numpy as np
+import matplotlib.pyplot as plt
 
 np.random.seed(5)
 
-# Data
 x = np.random.randn(2000) * 10
 y = np.random.randn(2000) * 10
 z = np.random.rand(2000) + 40 - np.sqrt(np.square(x) + np.square(y))
 
-# Plotting
-cbar.set_label("elevation (m)")
-plt.xlabel("x coordinate (m)")
-plt.title("Mountain Elevation")
-plt.ylabel("y coordinate (m)")
-plt.scatter(x, y, c=z)
-cbar = plt.colorbar()
 
-plt.show()
+# Create a scatter plot using a colormap
+scatter_plot = plt.scatter(x, y, s=40, c=z, cmap='viridis', marker='o', alpha=0.7)
+
+# Add a colorbar to show the intensity scale
+plt.colorbar(scatter_plot, label = 'elevation (m)')
+
+# Add labels and title to the plot (optional)
+plt.xlabel('x coordinate (m)')
+plt.ylabel('Y-axis')
+plt.title('Mountain y coordinate (m)')
+
+# Show the plot
+plt.show(block=True)
