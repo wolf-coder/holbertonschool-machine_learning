edc88c9038668b0d721ab7ef5c329f147f031194
Fakhri Baklouti <cuore.fakhri@gmail.com>
2024-05-12 19:13:12 +0100

    skip

--- math/0x01-plotting/6-bars.py
+++ math/0x01-plotting/6-bars.py
@@ -3,18 +3,6 @@ import numpy as np
 import matplotlib.pyplot as plt
 np.random.seed(5)
 fruit = np.random.randint(0, 20, (4, 3))
-"""
-categories = ['Farrah', 'Fred', 'Felicia']
-fruits = ['apples', 'bananas', 'oranges', 'peaches']
-colors = ['red', 'yellow', '#ff8000', '#ffe5b4']
-for i, fruit_name in enumerate(fruits):
-    fruit_values = fruit[i, :]
-    if i == 0:
-        plt.bar(categories, fruit_values, width=0.5, color=colors[i], label=fruit_name)
-    else:
-        bottom_fruit_values = np.sum(fruit[:i, :], axis=0)
-        plt.bar(categories, fruit_values, width=0.5, bottom=bottom_fruit_values, color=colors[i], label=fruit_name)
-"""
 categories = ['Farrah', 'Fred', 'Felicia']
 apples = fruit[0, :]
 bananas = fruit[1, :]
