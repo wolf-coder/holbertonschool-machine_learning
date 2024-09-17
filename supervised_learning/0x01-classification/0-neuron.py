c2390531c7f60f6af90138be4572a3141372be36
Fakhri Baklouti <cuore.fakhri@gmail.com>
2024-09-17 20:15:23 +0100

    minerva: commit

--- supervised_learning/0x01-classification/0-neuron.py
+++ supervised_learning/0x01-classification/0-neuron.py
@@ -9,14 +9,15 @@ class Neuron:
     """
     class Neuron that defines a single neuron performing binary classification
     """
-
-    def __init__(self, nx):
+    def __init__(self, nx) -> None:
         """
+        nx is the number of input features to the neuron.
         """
-        if type(nx) is not int:
+        if not isinstance(nx, int):
             raise TypeError("nx must be an integer")
         if nx < 1:
             raise ValueError("nx must be a positive integer")
-        self.W = np.random.randn(1, nx)
+
+        self.W = np.random.normal(size=(1, nx))
         self.b = 0
         self.A = 0
