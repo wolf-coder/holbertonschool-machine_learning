1ca6a1d2e2b819e4917dab824f4cba5ea96e0466
Fakhri Baklouti <cuore.fakhri@gmail.com>
2025-06-14 12:59:51 +0100

    autopep 1

--- supervised_learning/0x01-classification/1-neuron.py
+++ supervised_learning/0x01-classification/1-neuron.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """
-Neuron
+Neuron doc
 """
 import numpy as np
 
@@ -22,7 +22,7 @@ class Neuron:
         self.__W = np.random.normal(size=(1, nx))
         self.__b = 0
         self.__A = 0
-        
+
     @property
     def W(self):
         return self.__W
@@ -30,7 +30,7 @@ class Neuron:
     @property
     def b(self):
         return self.__b
-    
+
     @property
     def A(self):
         return self.__A
