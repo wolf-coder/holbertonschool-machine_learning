44d622ede06490144a02196724426339db5df52f
Fakhri Baklouti <cuore.fakhri@gmail.com>
2025-06-14 13:19:33 +0100

    apply NumPy-style docstring

--- supervised_learning/0x01-classification/0-neuron.py
+++ supervised_learning/0x01-classification/0-neuron.py
@@ -9,10 +9,36 @@ class Neuron:
     """
     class Neuron that defines a single neuron performing binary classification
     """
+
     def __init__(self, nx) -> None:
         """
-        nx is the number of input features to the neuron.
+        Initialize a single neuron for binary classification.
+
+        Parameters
+        ----------
+        nx : int
+            The number of input features to the neuron. Must be a positive integer.
+
+        Raises
+        ------
+        TypeError
+            If `nx` is not an integer.
+
+        ValueError
+            If `nx` is less than 1.
+
+        Attributes
+        ----------
+        __W : np.ndarray of shape (1, nx)
+            The weights vector, initialized using a normal distribution.
+
+        __b : float
+            The bias, initialized to 0.
+
+        __A : float
+            The activated output, initialized to 0.
         """
+
         if not isinstance(nx, int):
             raise TypeError("nx must be an integer")
         if nx < 1:
