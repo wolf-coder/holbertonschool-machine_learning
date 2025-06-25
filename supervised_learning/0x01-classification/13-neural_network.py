97efe770a3c43ebebef1b37b094200c26af9df23
Fakhri Baklouti <cuore.fakhri@gmail.com>
2025-06-25 23:04:15 +0100

    apply previous numpy code formatted style

--- supervised_learning/0x01-classification/13-neural_network.py
+++ supervised_learning/0x01-classification/13-neural_network.py
@@ -1,32 +1,84 @@
 #!/usr/bin/env python3
+"""
+Neural Network
+"""
 import numpy as np
+
 
 class NeuralNetwork():
     """
-    Neural network with one hiddden layer.
+    Neural network with one hidden layer for binary classification.
     """
+
     def __init__(self, nx, nodes):
         """
-    W1: The weights vector for the hidden layer. (node, nx)
-    b1: The bias for the hidden layer.(node, 1)
-    A1: The activated output for the hidden layer. (node, 1)
-    W2: The weights vector for the output neuron. (node, 1)
-    b2: The bias for the output neuron . (1)
-    A2: The activated output for the output neuron (prediction). (1)
+        Initialize the neural network.
+
+        Parameters
+        ----------
+        nx : int
+            Number of input features.
+            Must be a positive integer.
+
+        nodes : int
+            Number of nodes in the hidden layer.
+            Must be a positive integer.
+
+        Raises
+        ------
+        TypeError
+            If `nx` is not an integer.
+        ValueError
+            If `nx` is less than 1.
+        TypeError
+            If `nodes` is not an integer.
+        ValueError
+            If `nodes` is less than 1.
+
+        Attributes
+        ----------
+        W1 : np.ndarray of shape (nodes, nx)
+            Weights for the hidden layer,
+        initialized with a normal distribution.
+
+        b1 : np.ndarray of shape (nodes, 1)
+            Biases for the hidden layer, initialized with zeros.
+
+        A1 : float
+            Activated output for the hidden layer, initialized to 0.
+
+        W2 : np.ndarray of shape (1, nodes)
+            Weights for the output neuron,
+            initialized with a normal distribution.
+
+        b2 : float
+            Bias for the output neuron, initialized to 0.
+
+        A2 : float
+            Activated output for the output neuron, initialized to 0.
+
+        Notes
+        -----
+        The attributes are private and can be accessed via
+        the corresponding property methods:
+        - W1, b1, A1: Weights, bias, and activation from the hidden layer.
+        - W2, b2, A2: Weights, bias, and activation from the output neuron.
+
         """
+
+        #  nx: verification|assignment
         if not isinstance(nx, int):
             raise TypeError("nx must be an integer")
         if nx < 1:
-            raise ValueError("nx must be positive integer")
+            raise ValueError("nx must be a positive integer")
         self.nx = nx
 
         #  nodes: verification|assignment
         if not isinstance(nodes, int):
             raise TypeError("nodes must be an integer")
         if nodes < 1:
-            raise ValueError("nodes must be positive integer")
+            raise ValueError("nodes must be a positive integer")
         self.nodes = nodes
-
         self.__W1 = np.random.normal(size=(nodes, nx))
         self.__b1 = np.zeros(shape=(nodes, 1))
         self.__A1 = 0
@@ -34,7 +86,7 @@ class NeuralNetwork():
         self.__W2 = np.random.normal(size=(1, nodes))
         self.__b2 = 0
         self.__A2 = 0
-        
+
     @property
     def W1(self):
         return self.__W1
