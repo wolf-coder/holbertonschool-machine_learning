a52a925469678e5195f057b22d27fc272cbbfa16
Fakhri Baklouti <cuore.fakhri@gmail.com>
2025-06-25 23:53:39 +0100

    decoupling dZ1 and dA1

--- supervised_learning/0x01-classification/13-neural_network.py
+++ supervised_learning/0x01-classification/13-neural_network.py
@@ -214,20 +214,64 @@ class NeuralNetwork():
 
     def gradient_descent(self, X, Y, A1, A2, alpha=0.05):
         """
-        Performs one pass of gradient descent on the neural network        Performs one pass of gradient descent on the neural network
-        """
-        m = Y.shape[1]
+        Performs one pass of gradient descent on the neural network.
 
+        Parameters
+        ----------
+        X : np.ndarray of shape (nx, m)
+            Input data where:
+            - nx is the number of input features.
+            - m is the number of examples.
+
+        Y : np.ndarray of shape (1, m)
+            Correct labels for the input data.
+
+        A1 : np.ndarray of shape (nodes, m)
+            Activated output from the hidden layer.
+
+        A2 : np.ndarray of shape (1, m)
+            Activated output from the output layer (predicted output).
+
+        alpha : float, optional
+            Learning rate used in gradient descent (default is 0.05).
+
+        Updates
+        -------
+        __W1 : np.ndarray
+            Weights of the hidden layer, updated in-place.
+
+        __b1 : np.ndarray
+            Biases of the hidden layer, updated in-place.
+
+        __W2 : np.ndarray
+            Weights of the output layer, updated in-place.
+
+        __b2 : float
+            Bias of the output layer, updated in-place.
+        """
+
+        m = X.shape[1]
+        # calculate the derivatives
+
+# 1. gradient of the loss with respect to the output layer's activations:
         dZ2 = A2 - Y
-        dW2 = np.dot(dZ2, A1.T) / m
+
+# 2. gradient with respect to the weights and biases of the second layer:
+        dW2 = (dZ2 @ A1.T) / m
         db2 = np.sum(dZ2, axis=1, keepdims=True) / m
 
-        dA1 = np.dot(self.__W2.T, dZ2)
+# 3. gradient of the loss with respect to the first layer's activations:
+        dA1 = self.__W2.T @  dZ2
         dZ1 = dA1 * A1 * (1 - A1)
-        dW1 = np.dot(dZ1, X.T) / m
+
+# 4. gradient with respect to the weights and biases of the first layer:
+        dW1 = (dZ1 @ X.T) / m
         db1 = np.sum(dZ1, axis=1, keepdims=True) / m
 
-        self.__W1 -= alpha * dW1
-        self.__b1 -= alpha * db1
-        self.__W2 -= alpha * dW2
-        self.__b2 -= alpha * db2
+        # 5. Update the weights and biases for the second layer:
+        self.__W2 -= (alpha * dW2)
+        self.__b2 -= (alpha * db2)
+
+        # 6. Update the weights and biases for the first layer:
+        self.__W1 -= (alpha * dW1)
+        self.__b1 -= (alpha * db1)
